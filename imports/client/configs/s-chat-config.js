c18ed2f2c4780a481cf30a2868a006d817485f65
dalip123 <dalipkumar703@gmail.com>
2017-07-18 14:21:01 +0530

    update

--- imports/client/configs/s-chat-config.js
+++ imports/client/configs/s-chat-config.js
@@ -7,7 +7,7 @@ Meteor.startup(function () {
     sChat.init('vkY7LM7w9YmtGpsGe', {
         ssl: true,
         welcomeMessage: 'Hello, this is just a demo chat box, I\'m almost always offline ;) But if you have any questions, you can leave a message with your e-mail. Just send it as a message. I\'ll read it later and get back to you. There will be some improvements regarding this functionality soon!',
-        hostName: 'saas-web-bot.herokuapp.com',
+        hostName: '100708f7.ngrok.io',
         labels: {
             sendPlaceholder: 'Send the message... (Enter to send)',
             headerTitle: 'SimpleChat.Support '
